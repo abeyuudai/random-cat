9d120b9fbbc611330b389bdeb28abab887e0b36f
Abe Yudai <abeyuudai@gmail.com>
2024-12-19 22:02:33 +0900

    Change buton label into english

--- pages/index.tsx
+++ pages/index.tsx
@@ -29,7 +29,7 @@ const IndexPage: NextPage = () => {
     return (
         <div className={styles.page}>
             <button onClick={handleClick} className={styles.button}>
-                他のにゃんこも見る
+                One more cat!!!
             </button>
             <div className={styles.frame}>
                 {loading || <img src={imageUrl} className={styles.img} />}
